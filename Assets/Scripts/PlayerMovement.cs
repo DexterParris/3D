433613d0ad10d18b767a31b26d54210fec8e6609
DexterParris <91732338+DexterParris@users.noreply.github.com>
2021-10-09 21:01:15 +0100

    added movement to the skateboard

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,27 +2,55 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
 public class PlayerMovement : MonoBehaviour
 {
+    private Rigidbody rb;
+    private Animator anim;
     // Start is called before the first frame update
     void Start()
     {
         //anim = GetComponent<Animator>();
-        //rb = GetComponent<Rigidbody>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
         DoMove();
+        DoJump();
     }
 
     void DoMove()
     {
         if(Input.GetKey("w"))
         {
-            //rb.AddForce(direction * 10);
+            rb.AddRelativeForce(Vector3.forward * 50);
             
+        }
+
+        if(Input.GetKey("d"))
+        {
+            rb.AddRelativeForce(Vector3.right *30);
+        }
+
+        if(Input.GetKey("a"))
+        {
+            rb.AddRelativeForce(Vector3.left *30);
+        }
+
+        if(Input.GetKey("s"))
+        {
+            rb.AddRelativeForce(Vector3.back *50);
+        }
+    }
+
+    void DoJump()
+    {
+        if(Input.GetKeyDown("space"))
+        {
+            rb.AddForce(Vector3.up * 2000);
         }
     }
 }
